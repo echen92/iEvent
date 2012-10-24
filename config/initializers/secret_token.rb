9d688511a4b9921a46addaa7f0ae23e4db184e8e
iFraggle <westley_mak@yahoo.com>
2012-10-24 18:34:51 -0500

    gemfiles + DB

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IEventProject::Application.config.secret_token = '01822b89011b83d879fdcdbd8188a03ffaa0224a22048a610121083759bd71ccd7d9737adbd53027677eb862ab0020cf83c3b93b01ee8995da413d58166f21d0'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,15 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_iEvent Project_session',
-  :secret      => '28467fd651b1ed76fd437ee52da406fbfc7e4e893eba9a385c068dd47e17dcdf2b13b18e393582efa9acb65ce703fa8fbd420bffdbfa9b52e1453c25ea59ddbf'
-}
+IEventProject::Application.config.session_store :cookie_store, key: '_iEvent Project_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+# (create the session table with "rails generate session_migration")
+# IEventProject::Application.config.session_store :active_record_store
