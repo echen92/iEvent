86ff21f9041c0771f075eea399fecf6d419e50ab
iFraggle <westley_mak@yahoo.com>
2012-10-24 17:06:40 -0500

    structures

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '627324d73ae50c8bbeeefde62343b405b156a836f9ffe2134a36bafb1ce13ac42810032c02ee93ab6a6ec507b5025901fc5b37953340fdad715527197b689708';
